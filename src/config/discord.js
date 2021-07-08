1299727b2345dbb1702202249eae3c500c0a3c45
PlutonusDev <plutonus.stream@gmail.com>
2021-07-08 06:13:32 +0000

    Let's just fix this up real quick...

--- src/config/discord.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	// Your bot's prefix.
-	prefix: "a!",
-
-	tokens: {
-		// Your Discord bot's token.
-		discord: "ODYyNTYzMjQ5NzM1NzI5MTYz.YOaKoA.5in8IE80G9zOVLgsLWo3dVp9vXs"
-	}
-}
