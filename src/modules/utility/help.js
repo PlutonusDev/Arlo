6f48c3ea8da31ed1a92ea98a3ba30bbe184b48a8
PlutonusDev <plutonus.stream@gmail.com>
2021-07-10 08:06:15 +0000

    Fixed embeds for Discord.JS v13

--- src/modules/utility/help.js
+++ src/modules/utility/help.js
@@ -15,36 +15,36 @@ module.exports = {
 			let out = `Use \`${arlo.config.prefix}help <Command>\` for more information on a specific command.\n\n`;
 			const { commands, categories } = arlo;
 			for (x of categories) {
-				out += `**${x.charAt(0).toUpperCase() + x.substr(1)} Commands**\n\`\`\`asciidoc\n${commands.filter(command => command.category === x).map(command => `${command.module.name}${" ".repeat(longest - command.module.name.length)} :: ${command.module.description}`).join('\n')}\`\`\`\n\n`
+				out += `**${x.charAt(0).toUpperCase() + x.substr(1)} Commands**\n\`\`\`asciidoc\n${commands.filter(command => command.category === x).map(command => `${command.module.name}${" ".repeat(longest - command.module.name.length)} :: ${command.module.description || "No description."}`).join('\n')}\`\`\`\n\n`
 			}
 			out += `For a more detailed list of commands, [click / tap here](https://azure.plutonus.co/commands).`;
-			return msg.reply({embed:{
+			return msg.reply({embeds:[{
 				author: {
 					name: "Commands List",
 					icon_url: ""
 				},
 				description: out
-			}});
+			}]});
 		} else {
 			let cmd = args[0];
 			if (arlo.commands.has(cmd) || arlo.commands.find((c) => c.module.aliases && c.module.aliases.includes(cmd))) {
 				command = arlo.commands.get(cmd) || azure.commands.find(c => c.module.aliases && c.module.aliases.includes(cmd));
 				const longest = 8;
-				return msg.reply({embed:{
+				return msg.reply({embeds:[{
 					author: {
 						name: "Command Help",
 						icon_url: ""
 					},
 					description: `\`\`\`asciidoc\nCommand${" ".repeat(longest-7)} :: ${command.module.name}\nCategory${" ".repeat(longest-8)} :: ${command.category}\nCooldown${" ".repeat(longest-8)} :: ${command.module.cooldown ? `${command.module.cooldown} Seconds` : "3 Seconds"}\n\n[ Usage ]\n${command.module.usage ? arlo.config.prefix+command.module.usage : "Not Specified"}\n\n[ Description ]\n${command.module.description ? command.module.description : "Not Specified"}\`\`\``
-				}});
+				}]});
 			} else {
-				return msg.reply({embed:{
+				return msg.reply({embeds:[{
 					author: {
 						name: "Invalid Command",
 						icon_url: ""
 					},
 					description: `\`${command}\` is not a valid command.`
-				}});
+				}]});
 			}
 		}
 	}
