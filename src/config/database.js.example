79beaae276bc91a4006eabfc2a5b6ad9b445536c
PlutonusDev <plutonus.stream@gmail.com>
2021-07-11 09:34:56 +0000

    Update config structures.

---
+++ src/config/database.js.example
@@ -0,0 +1,4 @@
+module.exports = {
+	// Your MongoDB address
+	address: "mongodb://localhost/arlo"
+}
--- src/config/discord.example.js
+++ src/config/discord.example.js
@@ -2,6 +2,14 @@ module.exports = {
 	// Your bot's prefix.
 	prefix: "a!",
 
+	oAuth2: {
+		// Your Discord application's Client ID.
+		ClientID: "",
+
+		// Your Discord application's Client Secret.
+		ClientSecret: ""
+	},
+
 	tokens: {
 		// Your Discord bot's token.
 		discord: ""
