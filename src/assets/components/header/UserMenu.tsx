877654a7b5c933ef862155d792a501c483c1397c
PlutonusDev <plutonus.stream@gmail.com>
2021-07-18 11:32:51 +0000

    Fix UserMenu component

--- src/assets/components/header/UserMenu.tsx
+++ src/assets/components/header/UserMenu.tsx
@@ -30,23 +30,23 @@ function UserMenu() {
 
 	return (
 		<div className="relative inline-flex">
-			<button ref={trigger}
-				className="inline-flex justify-center items-center group"
-				aria-haspopup="true"
-				onClick={() => setDropdownOpen(!dropdownOpen)}
-				aria-expanded={dropdownOpen}
-			>
-				{session ? <>
+			{session ? <>
+				<button ref={trigger}
+					className="inline-flex justify-center items-center group"
+					aria-haspopup="true"
+					onClick={() => setDropdownOpen(!dropdownOpen)}
+					aria-expanded={dropdownOpen}
+				>
 					<img className="w-8 h-8 rounded-full" src={session.user.image} width="32" height="32" alt="Profile Picture" />
 					<div className="flex items-center truncate">
 						<svg className="w-3 h-3 flex-shrink-0 ml-1 fill-current text-gray-400" viewBox="0 0 12 12">
 							<path d="M5.9 11.4L.5 6l1.4-1.4 4 4 4-4L11.3 6z" />
 						</svg>
 					</div>
-				</> : <>
-					<button className="bg-green-400 rounded-lg text-white px-2" onClick={() => signIn("discord")}>Sign In</button>
-				</>}
-			</button>
+				</button>
+			</> : <>
+				<button className="bg-green-400 rounded-sm text-white px-2" onClick={() => signIn("discord")}>Sign In</button>
+			</>}
 
 			{session && <>
 				<div className={`origin-top-right z-10 absolute top-full right-0 m-w-64 bg-gray-700 border-gray-200 py-1.5 rounded shadow-lg overflow-hidden mt-1 ${dropdownOpen ? "block" : "hidden"}`}>
